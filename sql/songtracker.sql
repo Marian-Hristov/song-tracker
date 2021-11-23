b4033c6c471e1cf77658e5e66da5d0cdfd41036d
Mauricio Murillo <mauriciodelcipote@hotmail.com>
2021-11-23 13:18:58 -0500

    updated sql build sript

--- sql/songtracker.sql
+++ sql/songtracker.sql
@@ -1,23 +1,60 @@
-drop table productionroles;
-drop table contributor;
+
+-- recordings / contributions
+drop table musicalContributions;
+drop table productionContributions;
+drop table productionRoles;
 drop table recordings;
+drop table contributors;
+drop table musicianRoles;
 
-
-create table contributor(
-    contributor_id char(5) primary key,
-    name varchar2(100)
+create table productionRoles(
+    role_id char(5) primary key,
+    role_name varchar2(100) not null
 );
 
-create table recordings(
+create table recordings (
     recording_id char(5) primary key,
-    creation_time timestamp(0)
+    name varchar2(100) not null,
+    create_time timestamp(0) not null,
+    duration number(5,1) not null,
+    constraint check_positive_duration_reconrding check(duration > 0)
 );
 
-create table productionroles (
+create table contributors (
+    contributor_id char(5) primary key,
+    name varchar2(100) not null
+);
+
+create table productionContributions (
     recording_id char(5),
     contributor_id char(5),
-    role_name varchar2(100),
-    foreign key (recording_id) references recordings (recording_id)
+    role_id char(5),
+    foreign key (recording_id) references recordings (recording_id),
+    foreign key (contributor_id) references contributors (contributor_id),
+    foreign key (role_id) references productionRoles (role_id)
 );
 
+create table musicianRoles (
+    role_id char(5) primary key,
+    name varchar2(100) not null
+);
 
+create table musicalContributions (
+    recording_id char(5),
+    contributor_id char(5),
+    role_id char(5),
+    foreign key (recording_id) references recordings (recording_id),
+    foreign key (contributor_id) references contributors (contributor_id),
+    foreign key (role_id) references musicianroles (role_id)
+);
+
+-- compilations / samples
+drop table compilations;
+
+create table compilations (
+    compilation_id char(5) primary key,
+    name varchar2(100) not null,
+    creation_time timestamp(0) not null,
+    duration number(5,1) not null,
+    constraint check_positive_duration_compilation check(duration > 0)
+);
