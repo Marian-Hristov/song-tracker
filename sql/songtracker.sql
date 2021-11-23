1a00d29ce163f0717a0b823a39e1b8e4a5987c68
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-11-23 14:18:54 -0500

    finished create table statements

--- sql/songtracker.sql
+++ sql/songtracker.sql
@@ -17,7 +17,7 @@ create table recordings (
     name varchar2(100) not null,
     create_time timestamp(0) not null,
     duration number(5,1) not null,
-    constraint check_positive_duration_reconrding check(duration > 0)
+    constraint check_positive_duration_recording check(duration > 0)
 );
 
 create table contributors (
@@ -50,6 +50,9 @@ create table musicalContributions (
 
 -- compilations / samples
 drop table compilations;
+drop table segment;
+drop table recordingsamples;
+drop table compilationsamples;
 
 create table compilations (
     compilation_id char(5) primary key,
@@ -57,4 +60,74 @@ create table compilations (
     creation_time timestamp(0) not null,
     duration number(5,1) not null,
     constraint check_positive_duration_compilation check(duration > 0)
+);
+
+create table segment (
+    segment_id char(5) primary key,
+    main_track_offset number(5,1) not null,
+    duration_in_main_track number(5,1) not null,
+    component_track_offset number(5,1) not null,
+    duration_of_component number(5,1) not null,
+    constraint check_positive_main_track_offset_segment check(main_track_offset >= 0),
+    constraint check_postive_duration_in_main_track_segment check(duration_in_main_track > 0),
+    constraint check_positive_component_track_offset_segment check(component_track_offset >= 0),
+    constraint check_positive_duration_of_component check(duration_of_component > 0)
+);
+
+create table recordingsamples (
+    compilation_id char(5),
+    recording_id char(5),
+    segment_id char(5),
+    foreign key (compilation_id) references compilations (compilation_id),
+    foreign key (recording_id) references recordings (recording_id),
+    foreign key (segment_id) references segment (segment_id)
+);
+
+create table compilationsamples (
+    compilation_id char(5),
+    compilation_used char(5),
+    segment_id char(5),
+    foreign key (compilation_id) references compilations (compilation_id),
+    foreign key (compilation_used) references compilations (compilation_id),
+    foreign key (segment_id) references segment (segment_id)
+);
+
+-- Album / Distribution
+drop table recordlabels;
+drop table markets;
+drop table collections;
+drop table distributions;
+drop table collectioncompilations;
+
+create table recordlabels (
+    label_id char(5) primary key,
+    name varchar2(100) not null
+);
+
+create table markets (
+    market_id char(5) primary key,
+    name varchar2(100) not null
+);
+
+create table collections (
+    collection_id char(5) primary key,
+    name varchar2(100) not null
+);
+
+create table distributions (
+    distribution_id char(5) primary key,
+    collection_id char(5),
+    release_date date not null,
+    label_id char(5),
+    market_id char(5),
+    foreign key (collections) references collections (collection_id),
+    foreign key (recordlabels) references recordlabels (label_id),
+    foreign key (markets) references markets (market_id)
+);
+
+create table collectioncompilations (
+    collection_id char(5),
+    compilation_id char(5),
+    foreign key (collections) references collections (collection_id),
+    foreign key (compilations) references compilations (compilation_id)
 );
