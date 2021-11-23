10ab52d7cda082a48c0d7fe071b923e2038cf39e
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-11-23 15:09:48 -0500

    sequences for all pk ids

--- sql/songtracker.sql
+++ sql/songtracker.sql
@@ -8,7 +8,7 @@ drop table contributors;
 drop table musicianRoles;
 
 create table productionRoles(
-    role_id char(5) primary key,
+    role_id char(5) primary key default,
     role_name varchar2(100) not null
 );
 
