b0f70881093d8a7b8a771b70c1ccda1560124a61
Marian <marian.hristov@dawsoncollege.qc.ca>
2021-11-24 16:19:26 -0500

    found a logical problem, solution requires assumption, check roles.sql line 32

--- sql/packages/roles.sql
+++ sql/packages/roles.sql
@@ -29,9 +29,38 @@ create or replace package body role_mgmt as
             raise_application_error(-20001, 'one or more arguments are null or empty');
         end if;
         if category = 'musician' then
+            -- Major problem, when we delete a contribution from a recording
+            -- that only has one contribution, then we need to delete that 
+            -- recording and if the recording is part of a sample that is 
+            -- the only sample in a compilation, then we need to delete that
+            -- compilation, and if that compilation is the only part of a 
+            -- collection, then we need to delete that collection, which 
+            -- means that we need to delete the associated distribution 
+
+            -- this is the chain
+            -- distribution -> collections -> collectioncompilations
+            -- -> compilations -> samples -> recording -> contributor
+
+            -- we would also need to repeat each of the steps above for
+            -- each element when we're going up the chain
+
+            -- solution: we allow each of the component to be stand alone
+            -- until the user wants to link them together, meaning that 
+            -- we can have a role that is not associated to any contributor
+            -- or a contributor not associated to any recording, and even
+            -- recordings that don't have associated contributors, we can 
+            -- continue this way too with compilations, samples, segments,
+            -- collections and distributions
+
+            -- this makes sense because not all recordings are used in a 
+            -- compilation, some may not be used but still kept in the DB
+            delete from musicalcontributions
+            where contributor_id = deleted_role_id;
             delete from musicianRoles
             where role_id = deleted_role_id;
         elsif category = 'production' then
+            delete from productioncontributions
+            where contributor_id = deleted_role_id;
             delete from productionRoles
             where role_id = deleted_role_id;
         else
