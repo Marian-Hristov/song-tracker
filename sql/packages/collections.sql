9fe1a1e52c3791b8f0993aec50a8a9eeddaa0213
Mauricio Murillo <mauriciodelcipote@hotmail.com>
2021-11-25 22:39:29 -0500

    started working on deleting the collections

--- sql/packages/collections.sql
+++ sql/packages/collections.sql
@@ -27,6 +27,23 @@ create or replace package body collection_mgmt as
         if (collection_id < 1 or compilation_id < 1) then
             raise_application_error(-20001, 'the collection_id or the compilation_id is bellow 1');
         end if;
+        for compilation in (select * from collectionCompilations) loop
+            
+        end loop;
+        -- TODO: check if that compilation is already in the collection
         insert into collectionCompilations values (collection_id, compilation_id);
+    end;
+
+    procedure removeCompilationFromCollection(
+        collection_id in collections.collection_id%type,
+        compilation_id in compilations.compilation_id%type
+    )
+    as
+    begin
+        if (collection_id < 1 or compilation_id < 1) then
+            raise_application_error(-20001, 'the collection_id or the compilation_id is bellow 1');
+        end if;
+        delete from collectionCompilations where collection_id = collection_id and compilation_id = compilation_id;
+        -- TODO: throw an error if no compilation was deleted
     end;
 end collection_mgmt;
