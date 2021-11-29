485426ffe89a34a4bf5c9deee07a746cf95c2b2d
Mauricio Murillo <mauriciodelcipote@hotmail.com>
2021-11-29 09:33:28 -0500

    added the log triggers for the compilations/samples

--- sql/build.sql
+++ sql/build.sql
@@ -260,7 +260,8 @@ create table collectioncompilations (
 -- login
 create table STLogs (
     log_id number(5) default log_id_seq.nextval primary key,
-    log_message varchar2(1000) not null
+    log_message varchar2(1000) not null,
+    log_time timestamp default localTimestamp(0) not null
 );
 -- triggers
 create or replace trigger before_insert_update_delete_compilations
@@ -279,14 +280,14 @@ begin
     if updating then
         
         insert into STLogs (log_message) values (user ||
-        ' updated table compilations. Old values compilation_id: '||:new.compilation_id||
-        ', compilation_name: '||:new.compilation_name||
-        ', creation_time: '||:new.creation_time||
-        ', duration: '||:new.duration||
-        '. New values compilation_id: '||:old.compilation_id||
+        ' updated table compilations. Old values compilation_id: '||:old.compilation_id||
         ', compilation_name: '||:old.compilation_name||
         ', creation_time: '||:old.creation_time||
-        ', duration: '||:old.duration);
+        ', duration: '||:old.duration||
+        '. New values compilation_id: '||:new.compilation_id||
+        ', compilation_name: '||:new.compilation_name||
+        ', creation_time: '||:new.creation_time||
+        ', duration: '||:new.duration);
     end if;
 
     if deleting then
@@ -296,6 +297,163 @@ begin
         ', creation_time: '||:old.creation_time||
         ', duration: '||:old.duration);
     end if;
+end;
 
+create or replace trigger before_insert_update_delete_compilationcontributions
+before insert or update or delete
+on compilationContributions
+for each row
+declare
+begin
+    if inserting then
+        insert into STLogs (log_message) values (user ||
+        ' inserted into table compilationContributions values compilation_id: '||:new.compilation_id||
+        ', contributor_id: '||:new.contributor_id||
+        ', role_id: '||:new.role_id);
+    end if;
+    if updating then
+
+        insert into STLogs (log_message) values (user ||
+        ' updated table compilationContributions. Old values compilation_id: '||:old.compilation_id||
+        ', contributor_id: '||:old.contributor_id||
+        ', role_id: '||:old.role_id||
+        '. New values compilation_id: '||:new.compilation_id||
+        ', contributor_id: '||:new.contributor_id||
+        ', role_id: '||:new.role_id);
+    end if;
+
+    if deleting then
+        insert into STLogs (log_message) values (user ||
+        ' deleted from table compilationContributions values compilation_id: '||:old.compilation_id||
+        ', contributor_id: '||:old.contributor_id||
+        ', role_id: '||:old.role_id);
+    end if;
+end;
+
+create or replace trigger before_insert_update_delete_compilationRoles
+before insert or update or delete
+on compilationRoles
+for each row
+declare
+begin
+    if inserting then
+        insert into STLogs (log_message) values (user ||
+        ' inserted into table compilationRoles values role_id: '||:new.role_id||
+        ', role_name: '||:new.role_name);
+    end if;
+    if updating then
+
+        insert into STLogs (log_message) values (user ||
+        ' updated table compilationRoles. Old values role_id: '||:old.role_id||
+        ', role_name: '||:old.role_name||
+        '. New values role_id: '||:new.role_id||
+        ', role_name: '||:new.role_name);
+    end if;
+
+    if deleting then
+        insert into STLogs (log_message) values (user ||
+        ' deleted from table compilationRoles values role_id: '||:old.role_id||
+        ', role_name: '||:old.role_name);
+    end if;
+end;
+
+create or replace trigger before_insert_update_delete_recordingSamples
+before insert or update or delete
+on recordingSamples
+for each row
+declare
+begin
+    if inserting then
+        insert into STLogs (log_message) values (user ||
+        ' inserted into table recordingSamples values compilation_id: '||:new.compilation_id||
+        ', recording_id: '||:new.recording_id||
+        ', segment_id: '||:new.segment_id);
+    end if;
+    if updating then
+
+        insert into STLogs (log_message) values (user ||
+        ' updated table recordingSamples. Old values compilation_id: '||:old.compilation_id||
+        ', recording_id: '||:old.recording_id||
+        ', segment_id: '||:old.segment_id||
+        '. New values compilation_id: '||:new.compilation_id||
+        ', recording_id: '||:new.recording_id||
+        ', segment_id: '||:new.segment_id);
+    end if;
+
+    if deleting then
+        insert into STLogs (log_message) values (user ||
+        ' deleted from table recordingSamples values compilation_id: '||:old.compilation_id||
+        ', recording_id: '||:old.recording_id||
+        ', segment_id: '||:old.segment_id);
+    end if;
+end;
+
+create or replace trigger before_insert_update_delete_compilationSamples
+before insert or update or delete
+on compilationSamples
+for each row
+declare
+begin
+    if inserting then
+        insert into STLogs (log_message) values (user ||
+        ' inserted into table compilationSamples values compilation_id: '||:new.compilation_id||
+        ', compilation_used: '||:new.compilation_used||
+        ', segment_id: '||:new.segment_id);
+    end if;
+    if updating then
+
+        insert into STLogs (log_message) values (user ||
+        ' updated table compilationSamples. Old values compilation_id: '||:old.compilation_id||
+        ', compilation_used: '||:old.compilation_used||
+        ', segment_id: '||:old.segment_id||
+        '. New values compilation_id: '||:new.compilation_id||
+        ', compilation_used: '||:new.compilation_used||
+        ', segment_id: '||:new.segment_id);
+    end if;
+
+    if deleting then
+        insert into STLogs (log_message) values (user ||
+        ' deleted from table compilationSamples values compilation_id: '||:old.compilation_id||
+        ', compilation_used: '||:old.compilation_used||
+        ', segment_id: '||:old.segment_id);
+    end if;
+end;
+
+create or replace trigger before_insert_update_delete_segment
+before insert or update or delete
+for each row
+declare
+begin
+    if inserting then
+        insert into STLogs (log_message) values (user ||
+        ' inserted into table segment values segment_id: '||:new.segment_id||
+        ', main_track_offset: '||:new.main_track_offset||
+        ', duration_in_main_track: '||:new.duration_in_main_track||
+        ', component_track_offset: '||:new.component_track_offset||
+        ', duration_of_component: '||:new.duration_of_component);
+    end if;
+    if updating then
+
+        insert into STLogs (log_message) values (user ||
+        ' updated table segment. Old values segment_id: '||:old.segment_id||
+        ', main_track_offset: '||:old.main_track_offset||
+        ', duration_in_main_track: '||:old.duration_in_main_track||
+        ', component_track_offset: '||:old.component_track_offset||
+        ', duration_of_component: ':old.duration_of_component
+        '. New values segment_id: '||:new.segment_id||
+        ', main_track_offset: '||:new.main_track_offset||
+        ', duration_in_main_track: '||:new.duration_in_main_track||
+        ', component_track_offset: '||:new.component_track_offset||
+        ', duration_of_component: '||:new.duration_of_component);
+    end if;
+
+    if deleting then
+        insert into STLogs (log_message) values (user ||
+        ' deleted from table segment values segment_id: '||:old.segment_id||
+        ', main_track_offset: '||:old.main_track_offset||
+        ', duration_in_main_track: '||:old.duration_in_main_track||
+        ', component_track_offset: '||:old.component_track_offset||
+        ', duration_of_component: '||:old.duration_of_component);
+    end if;
 end;
 commit;
