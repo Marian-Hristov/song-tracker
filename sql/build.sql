da6bb703cfcc689825d4690cfa497fcddae987d4
Marian-Hristov <marianhr2014@gmail.com>
2021-12-03 23:56:16 -0500

    added defaults to build script

--- sql/build.sql
+++ sql/build.sql
@@ -833,3 +833,84 @@ begin
 end;
 /
 commit;
+/
+insert into markets (market_name) values ('Asia');
+insert into markets (market_name) values ('Africa');
+insert into markets (market_name) values ('Europe');
+insert into markets (market_name) values ('North-America');
+insert into markets (market_name) values ('South-America'); -- 5
+insert into markets (market_name) values ('Worldwide');
+commit;
+insert into musicianRoles (role_name) values ('accordionist');
+insert into musicianRoles (role_name) values ('piper');
+insert into musicianRoles (role_name) values ('banjoist');
+insert into musicianRoles (role_name) values ('bongosero');
+insert into musicianRoles (role_name) values ('bassist'); -- 5
+insert into musicianRoles (role_name) values ('bassoonist');
+insert into musicianRoles (role_name) values ('clarinetist');
+insert into musicianRoles (role_name) values ('cellist');
+insert into musicianRoles (role_name) values ('drummer');
+insert into musicianRoles (role_name) values ('euphoniumist'); -- 10
+insert into musicianRoles (role_name) values ('flutist');
+insert into musicianRoles (role_name) values ('guitarist');
+insert into musicianRoles (role_name) values ('harpist');
+insert into musicianRoles (role_name) values ('harpsichordist');
+insert into musicianRoles (role_name) values ('hornist'); -- 15
+insert into musicianRoles (role_name) values ('keyboardist');
+insert into musicianRoles (role_name) values ('lutenist');
+insert into musicianRoles (role_name) values ('mandolinist');
+insert into musicianRoles (role_name) values ('marimbist');
+insert into musicianRoles (role_name) values ('oboist'); -- 20
+insert into musicianRoles (role_name) values ('organist');
+insert into musicianRoles (role_name) values ('percussionist');
+insert into musicianRoles (role_name) values ('pianist');
+insert into musicianRoles (role_name) values ('recorder player');
+insert into musicianRoles (role_name) values ('saxophonist'); -- 25
+insert into musicianRoles (role_name) values ('tocaores');
+insert into musicianRoles (role_name) values ('trumpeter');
+insert into musicianRoles (role_name) values ('tubaist');
+insert into musicianRoles (role_name) values ('ukulelist');
+insert into musicianRoles (role_name) values ('violist'); -- 30
+insert into musicianRoles (role_name) values ('violinist');
+insert into musicianRoles (role_name) values ('xylophonist');
+commit;
+insert into productionRoles (role_name) values ('composer');
+insert into productionRoles (role_name) values ('remixer');
+insert into productionRoles (role_name) values ('recording engineer');
+insert into productionRoles (role_name) values ('producer');
+insert into productionRoles (role_name) values ('co-producer'); -- 5
+insert into productionRoles (role_name) values ('accompanist');
+insert into productionRoles (role_name) values ('arranger');
+insert into productionRoles (role_name) values ('audio engineer');
+insert into productionRoles (role_name) values ('vocalist');
+insert into productionRoles (role_name) values ('backing singer'); -- 10
+insert into productionRoles (role_name) values ('conductor');
+insert into productionRoles (role_name) values ('mixer');
+insert into productionRoles (role_name) values ('mastering engineer');
+insert into productionRoles (role_name) values ('sound designer');
+insert into productionRoles (role_name) values ('performer'); -- 15
+insert into productionRoles (role_name) values ('writter');
+insert into productionRoles (role_name) values ('lyricist');
+insert into productionRoles (role_name) values ('assistant engineer');
+insert into productionRoles (role_name) values ('mixing assistant');
+commit;
+insert into compilationRoles (role_name) values ('composer');
+insert into compilationRoles (role_name) values ('remixer');
+insert into compilationRoles (role_name) values ('recording engineer');
+insert into compilationRoles (role_name) values ('producer');
+insert into compilationRoles (role_name) values ('co-producer'); -- 5
+insert into compilationRoles (role_name) values ('accompanist');
+insert into compilationRoles (role_name) values ('arranger');
+insert into compilationRoles (role_name) values ('audio engineer');
+insert into compilationRoles (role_name) values ('vocalist');
+insert into compilationRoles (role_name) values ('backing singer'); -- 10
+insert into compilationRoles (role_name) values ('conductor');
+insert into compilationRoles (role_name) values ('mixer');
+insert into compilationRoles (role_name) values ('mastering engineer');
+insert into compilationRoles (role_name) values ('sound designer');
+insert into compilationRoles (role_name) values ('performer'); -- 15
+insert into compilationRoles (role_name) values ('writter');
+insert into compilationRoles (role_name) values ('lyricist');
+insert into compilationRoles (role_name) values ('assistant engineer');
+insert into compilationRoles (role_name) values ('mixing assistant');
+commit;
